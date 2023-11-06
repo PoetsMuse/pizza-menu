736811b2a1998e16e66878df1193a872d7d9dab9
Kate Glushkova <weirdis1@gmail.com>
2023-11-06 13:17:18 +0200

    Building UI out of components

--- src/index.js
+++ src/index.js
@@ -49,13 +49,46 @@ const pizzaData = [
 
 function App() {
     return (
-    <div>
-        <h1>Hello React! </h1>
-        <Pizza />
-        <Pizza />
-        <Pizza />
-    </div>);
+        <div>
+            <Header />
+            <Menu />
+            <Footer />
+        </div>);
 }
+
+function Header(){
+    return <h1>Fast React Pizza Co.</h1>;
+}
+function Menu(){
+    return (
+        <div>
+            <h2>Our Menu:</h2>
+            <Pizza />
+            <Pizza />
+            <Pizza />
+            <Pizza />
+            <Pizza />
+            <Pizza />
+        </div>)
+}
+function Footer(){
+    //without jsx
+    //return React.createElement('footer', null, "We're currently open");
+
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+    console.log(isOpen);
+
+    // if(hour >= openHour && hour <= closeHour) alert("We're currently open."); else alert("Sorry, we're closed.")
+
+    return <footer>{new Date().toLocaleTimeString()}We're currently open.</footer>;
+}
+
+// const Test = () => {
+
+// }
 
 function Pizza() {
     return (
